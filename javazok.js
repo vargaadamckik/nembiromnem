36d6580bfc383597a7dd6bd9a6f817934be20380
Csemoloverforever <122789211+vargaadamckik@users.noreply.github.com>
2025-05-27 08:44:55 +0200

    Működő upgrade 1. Szeretek most már élni.

--- javazok.js
+++ javazok.js
@@ -2,6 +2,7 @@ var img = document.getElementById("meme");
 var counter = document.getElementById("szam");
 var count = parseInt(counter.innerHTML, 10);
 let ff = false;
+var changecount = 0;
 document.getElementById("auto1").style.display = "none";
 document.getElementById("bc").style.display = "none";
 kepcount = 0;
@@ -9,7 +10,12 @@ kepcount = 0;
 function incrementalok() {
     if (!ff) {
         img.addEventListener("click", function() {
-            count++;
+            if (changecount == 0) {
+                count++;
+            }
+            else {
+                count += changecount;
+            }
             counter.innerHTML = count;
         });
         ff = true;
@@ -37,5 +43,18 @@ function kepcsere() {
     else {
         kepturn.classList.remove("lightmode");
         kepturn.classList.add("darkmode");
+    }
+}
+
+function upgradek() {
+    let arszam = document.getElementById("arszam");
+    if (count >= arszam.innerText)  {
+        if (changecount == 0) {
+            changecount += 2;
+        }
+        else {
+            changecount *= 2;
+        }
+        arszam.innerText = arszam.innerText * 3;
     }
 }
