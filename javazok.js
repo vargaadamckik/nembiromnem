f5ccdd4279250a46a91fe9caa2fae31d38bfbb7c
Csemoloverforever <122789211+vargaadamckik@users.noreply.github.com>
2025-05-28 10:35:14 +0200

    upgrade 2, upgrade 3 működő képes. valamiért most már nem működik a nerf. XD

--- javazok.js
+++ javazok.js
@@ -4,7 +4,10 @@ var count = parseInt(counter.innerHTML, 10);
 let ff = false;
 let up3 = false;
 let b3 = document.getElementById("boost3");
+const matek = Math.floor((Math.round(Math.log10(count) * 2) * (upbg / 1.5)));
 var changecount = 0;
+var smallcountbuff = 1;
+var upbg = 0;
 var kattosido = 200;
 document.getElementById("auto1").style.display = "none";
 document.getElementById("bc").style.display = "none";
@@ -23,10 +26,17 @@ function incrementalok() {
 
             if (changecount == 0) {
                 count++;
-            } else {
-                count += changecount;
+            } else {              
+                if (up3) {
+                    const matek = Math.floor((Math.round(Math.log10(count) * 2) / (upbg / 5)));
+                    count += (changecount * matek) * smallcountbuff;
+                    b3.innerText = matek;
+                }
+                else {
+                    count += changecount * smallcountbuff;
+                }
             }
-            counter.innerHTML = count;
+            counter.innerHTML = Math.floor(Math.round(count));
         });
         ff = true;
         designolok()       
@@ -66,6 +76,7 @@ function upgrade1() {
     let booster = document.getElementById("boost");
     if (count >= arszam.innerText)  {
         count -= arszam.innerText;
+        upbg += 1;
         if (count <= 100000) {           
             if (changecount == 0) {
             changecount += 2;
@@ -78,18 +89,27 @@ function upgrade1() {
             changecount *= 1.5;
             document.getElementById("nerf").style.display = "block";
         } 
-        arszam.innerText = arszam.innerText * 3.5;
+        arszam.innerText = Math.floor(Math.round(arszam.innerText * 3.15));
         booster.innerText = changecount;
     }
 }
 
 function upgrade2() {
-
+    let arszam2 = document.getElementById("arszam2");
+    let boost2 = document.getElementById("boost2");
+    if (count >= arszam2.innerText) {
+        count -= arszam2.innerText;
+        upbg += 1;
+        smallcountbuff *= 1.1;      
+    }
+    arszam2.innerText = Math.floor(Math.round(arszam2.innerText * 2));
+    boost2.innerText = smallcountbuff;
 }
 
 function upgrade3() {
     let kszam = document.getElementById("kszam");
     if (count >= kszam.innerText)  {
+        upbg += 1;
         count -= kszam.innerText;
         kszam.innerText = "Megvéve"
         up3 = true;           
